e55a632a22a7fad033f1f78045034b160ef7524d
Rafael Moraes <rafaelbuenomoraes89@gmail.com>
2024-11-21 10:40:24 -0300

    Release: v2025.07.20

--- root/app/extended-scripts/untitled_album_cover.py
+++ root/app/extended-scripts/untitled_album_cover.py
@@ -14,18 +14,29 @@ def check_and_copy_images():
         # verifica se o nome do diretório atual é "untitled_album"
         if os.path.basename(root) == "untitled_album":
             print(
-                f"[cruix-music-archiver] 'untitled_album' detected, applying 'Various Artists' cover. time to give this album some personality! 🎤 🎶 : {root}")
+                f"[cruix-music-archiver] 'untitled_album' detected, applying 'untitled_album' cover. time to give this album some personality! 🎤 🎶 : {root}")
+
+            # verifica a existência dos arquivos padrão
+            print(f"Checking if {default_cover} exists: {os.path.exists(default_cover)}")
+            print(f"Checking if {default_folder} exists: {os.path.exists(default_folder)}")
+            print(f"Checking write access to {root}: {os.access(root, os.W_OK)}")
 
             # copia o arquivo cover.jpg, sobrescrevendo se necessário
             if os.path.exists(default_cover):
-                shutil.copy(default_cover, os.path.join(root, "cover.jpg"))
-                print(
-                    f"[cruix-music-archiver] copied: cover.jpg for {root}. now it's shining like a Grammy winner! 🏆 ✨")
+                try:
+                    shutil.copy(default_cover, os.path.join(root, "cover.jpg"))
+                    print(
+                        f"[cruix-music-archiver] copied: cover.jpg for {root}. now it's shining like a Grammy winner! 🏆 ✨")
+                except Exception as e:
+                    print(f"Error copying cover.jpg to {root}: {e}")
 
             # copia o arquivo folder.jpg, sobrescrevendo se necessário
             if os.path.exists(default_folder):
-                shutil.copy(default_folder, os.path.join(root, "folder.jpg"))
-                print(f"[cruix-music-archiver] copied: folder.jpg for {root}. folder just got a major glow-up! 💫 🎵")
+                try:
+                    shutil.copy(default_folder, os.path.join(root, "folder.jpg"))
+                    print(f"[cruix-music-archiver] copied: folder.jpg for {root}. folder just got a major glow-up! 💫 🎵")
+                except Exception as e:
+                    print(f"Error copying folder.jpg to {root}: {e}")
 
 
 if __name__ == "__main__":
