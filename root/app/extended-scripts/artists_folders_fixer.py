0d715e17e292221f09d4e2a35273cbdbb90694b8
Rafael Moraes <rafaelbuenomoraes89@gmail.com>
2024-11-25 17:15:53 -0300

    Release: v2025.09.01

--- root/app/extended-scripts/artists_folders_fixer.py
+++ root/app/extended-scripts/artists_folders_fixer.py
@@ -16,30 +16,28 @@ def load_replacements(replacements_path):
     with open(absolute_path, 'r', encoding='utf-8') as f:
         return [line.strip().split('|') for line in f.readlines() if line.strip()]
 
-def rename_direct_folders(music_directory, replacements):
+def rename_direct_folders(music_directory, old, new):
     any_renamed = False
     try:
         for folder_name in os.listdir(music_directory):
             folder_path = os.path.join(music_directory, folder_name)
-            if os.path.isdir(folder_path):
-                for old, new in replacements:
-                    if old in folder_name:
-                        new_folder_name = folder_name.replace(old, new)
-                        new_folder_path = os.path.join(music_directory, new_folder_name)
-                        if os.path.exists(folder_path):
-                            try:
-                                # temporary renaming if names only differ in case
-                                if folder_path.lower() == new_folder_path.lower():
-                                    temp_path = os.path.join(music_directory, new_folder_name + "_temp")
-                                    os.rename(folder_path, temp_path)
-                                    os.rename(temp_path, new_folder_path)
-                                else:
-                                    os.rename(folder_path, new_folder_path)
-                                logging.debug(f"renaming directory '{folder_name}' to '{new_folder_name}'")
-                                print(f"[cruix-music-archiver] fixed: '{folder_name}' to '{new_folder_name}' ♻️ ")
-                                any_renamed = True
-                            except FileNotFoundError as e:
-                                logging.error(f"error renaming directory '{folder_name}': {e}")
+            if os.path.isdir(folder_path) and old in folder_name:
+                new_folder_name = folder_name.replace(old, new)
+                new_folder_path = os.path.join(music_directory, new_folder_name)
+                if os.path.exists(folder_path):
+                    try:
+                        # temporary renaming if names only differ in case
+                        if folder_path.lower() == new_folder_path.lower():
+                            temp_path = os.path.join(music_directory, new_folder_name + "_temp")
+                            os.rename(folder_path, temp_path)
+                            os.rename(temp_path, new_folder_path)
+                        else:
+                            os.rename(folder_path, new_folder_path)
+                        logging.debug(f"renaming directory '{folder_name}' to '{new_folder_name}'")
+                        print(f"[cruix-music-archiver] fixed: '{folder_name}' to '{new_folder_name}' ♻️ ")
+                        any_renamed = True
+                    except FileNotFoundError as e:
+                        logging.error(f"error renaming directory '{folder_name}': {e}")
     except Exception as e:
         logging.error(f"error in renaming folders in '{music_directory}': {e}")
     return any_renamed
@@ -55,11 +53,12 @@ def main():
     print("[cruix-music-archiver] fixing artists' folders... 🔧  time to tidy up and make everything look perfect! 🔧  ")
     logging.debug("fixing artists folders...")
 
-    # continuously loop until no renaming occurs
-    while True:
-        renamed = rename_direct_folders(MUSIC_DIR, replacements)
-        if not renamed:
-            break
+    # loop through each replacement rule
+    for old, new in replacements:
+        while True:
+            renamed = rename_direct_folders(MUSIC_DIR, old, new)
+            if not renamed:
+                break
 
     print("[cruix-music-archiver] artists' folders fixed successfully! ⚡  mission accomplished, folders upgraded! ⚡  ")
     logging.debug("artists folders fixed successfully.")
