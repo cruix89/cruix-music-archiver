7804227d758b135ed1c0c06cc97aeb70db7b887f
Rafael Moraes <rafaelbuenomoraes89@gmail.com>
2024-11-29 08:27:14 -0300

    Release: v2025.10.08

--- root/app/extended-scripts/downloads_invalid_characters_remover.py
+++ root/app/extended-scripts/downloads_invalid_characters_remover.py
@@ -90,7 +90,7 @@ def main( download_path, lists_path ):
     rename_files_and_dirs(download_path, invalid_chars)
 
 # fixed print in terminal
-print("[cruix-music-archiver] the evil invalid defeated successfully... 🛡️  ⚔️  the cleanup force is strong! ✨")
+print("[cruix-music-archiver] The Evil Invalid Defeated Successfully... 🛡️  ⚔️  The Cleanup Force Is Strong! ✨")
 
 if __name__ == "__main__":
 
